94f1082bbe5df6189b17376090525e8553eb8ad6
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-16 08:31:04 +0530

    Add Rigetti Novera hardware page component

---
+++ app/hardware/rigetti-novera/page.tsx
@@ -0,0 +1,10 @@
+import HardwarePage from "@/components/HardwarePage";
+import { getProcessorBySlug } from "@/lib/content/hardware";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const processor = getProcessorBySlug("rigetti-novera");
+  if (!processor) return notFound();
+  return <HardwarePage processor={processor} />;
+}
+
