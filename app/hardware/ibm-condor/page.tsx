912cba85024d807df4833554ca6fec6174278820
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-16 08:28:49 +0530

    Create IBM Condor hardware page component

---
+++ app/hardware/ibm-condor/page.tsx
@@ -0,0 +1,10 @@
+import HardwarePage from "@/components/HardwarePage";
+import { getProcessorBySlug } from "@/lib/content/hardware";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const processor = getProcessorBySlug("ibm-condor");
+  if (!processor) return notFound();
+  return <HardwarePage processor={processor} />;
+}
+
