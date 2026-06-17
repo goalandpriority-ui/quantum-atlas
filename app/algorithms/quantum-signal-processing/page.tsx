a4b348bf0a38cf35b47010b402fbe88fb6d48c8d
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-17 20:10:07 +0530

    Add quantum signal processing algorithm page

---
+++ app/algorithms/quantum-signal-processing/page.tsx
@@ -0,0 +1,10 @@
+import AlgorithmPage from "@/components/AlgorithmPage";
+import { getAlgorithmBySlug } from "@/lib/content/algorithms";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const algorithm = getAlgorithmBySlug("quantum-signal-processing");
+  if (!algorithm) return notFound();
+  return <AlgorithmPage algorithm={algorithm} />;
+}
+
