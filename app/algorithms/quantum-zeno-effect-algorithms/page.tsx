0cca317a35c07813bb75c5dbb6da52357f5195ce
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-17 21:58:26 +0530

    Add page for quantum Zeno effect algorithms

---
+++ app/algorithms/quantum-zeno-effect-algorithms/page.tsx
@@ -0,0 +1,10 @@
+import AlgorithmPage from "@/components/AlgorithmPage";
+import { getAlgorithmBySlug } from "@/lib/content/algorithms";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const algorithm = getAlgorithmBySlug("quantum-zeno-effect-algorithms");
+  if (!algorithm) return notFound();
+  return <AlgorithmPage algorithm={algorithm} />;
+}
+
