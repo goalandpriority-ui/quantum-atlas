3ac835ced95b87ab280cb34b0c55d242aa0fc0b5
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-18 18:49:47 +0530

    Add measurement-collapse page component

---
+++ app/dictionary/measurement-collapse/page.tsx
@@ -0,0 +1,10 @@
+import TermPage from "@/components/TermPage";
+import { getTermBySlug } from "@/lib/content/dictionary";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const term = getTermBySlug("measurement-collapse");
+  if (!term) return notFound();
+  return <TermPage term={term} />;
+}
+
