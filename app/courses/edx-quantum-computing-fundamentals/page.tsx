ccb1bb877044e26751606816434131102326b205
goalandpriority-ui <goalandpriority@gmail.com>
2026-06-20 13:20:15 +0530

    Add course page for Quantum Computing Fundamentals

---
+++ app/courses/edx-quantum-computing-fundamentals/page.tsx
@@ -0,0 +1,10 @@
+import CoursePage from "@/components/CoursePage";
+import { getCourseBySlug } from "@/lib/content/courses-new";
+import { notFound } from "next/navigation";
+
+export default function Page() {
+  const course = getCourseBySlug("edx-quantum-computing-fundamentals");
+  if (!course) return notFound();
+  return <CoursePage course={course} />;
+}
+
